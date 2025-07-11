2826243d1056d7404c8dd495a6dd870c134c65c7
Donn Jerico Belmonte <belmontedonnjerico@gmail.com>
2025-07-11 23:09:41 +0800

    Add landing page, signin, signup and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "home/index"
+  devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,4 +14,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root 'home#index'
 end
